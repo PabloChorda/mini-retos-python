5cc00a1990f03c25a9e77bc4434402d55e4fbdaf
PabloChorda <pablochorda365@gmail.com>
2025-03-21 12:20:38 +0100

    Primera practica

---
+++ 52_simulador_abejas.py
@@ -0,0 +1,13 @@
+'''
+Simulador de Ecosistema de Abejas y Flores
+
+Descripción del ejercicio:
+Diseña una simulación en la que un jardín (una cuadrícula de 10x10) contiene flores y abejas.
+
+Flores: Aparecen en posiciones aleatorias y cada una tiene una cantidad de néctar.
+Abejas: Se mueven aleatoriamente por el jardín. Si una abeja se encuentra en la misma celda que una flor, 
+“chupa” parte del néctar y gana energía, mientras la flor lo pierde.
+Dinámica: Si una flor se queda sin néctar, se “regenera” automáticamente con un nuevo valor aleatorio.
+Salida: Cada iteración se muestra el estado del jardín en ASCII, indicando la posición de flores y abejas, durante 20 ciclos de simulación.
+'''
+
